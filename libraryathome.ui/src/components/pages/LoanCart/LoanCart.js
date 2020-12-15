b2800b3e0e15aa14091a7b930c131834ef811756
Kayla Melton <kannmelton2@gmail.com>
2020-12-14 18:33:28 -0800

    can view the borrower and book on the loan cart page

--- libraryathome.ui/src/components/pages/LoanCart/LoanCart.js
+++ libraryathome.ui/src/components/pages/LoanCart/LoanCart.js
@@ -6,6 +6,9 @@ import libraryItemData from '../../../helpers/data/libraryItemData';
 import loanData from '../../../helpers/data/loanData';
 import userData from '../../../helpers/data/userData';
 
+import LoanBooks from '../../shared/LoanBooks/LoanBooks';
+import SecondaryNav from '../../shared/SecondaryNav/SecondaryNav';
+
 import './LoanCart.scss';
 
 class LoanCart extends React.Component {
@@ -43,11 +46,41 @@ class LoanCart extends React.Component {
     }
 
     render() {
+        const { books, borrower } = this.state;
+
+        const buildLoanBooks = books.map((book) => (
+            <LoanBooks key={book.libraryItemId} book={book} />
+        ));
+
         return(
             <main className="LoanCart">
                 <header>
                     <h1>You are loaning...</h1>
                 </header>
+                <section className="container">
+                    <div className="row">
+                        <div className="col-3 secondary-nav">
+                            <header>
+                                Do Stuff
+                            </header>
+                            <SecondaryNav />
+                        </div>
+                        <div className="col-9 d-flex flex-wrap">
+                            <div className="loan-books">
+                                <header>
+                                    <p>You are loaning the following books:</p>
+                                </header>
+                            {buildLoanBooks}
+                            </div>
+                            <div className="loan-borrower">
+                                <header>
+                                    <p>You are loaning the above books to:</p>
+                                </header>
+                                <p className="card">{borrower.firstName} {borrower.lastName}</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         )
     }
